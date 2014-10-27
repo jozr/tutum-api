3d248722052736b5913ba3dcb287aec84b9d2188
jozw <jozwright@gmail.com>
2014-10-27 12:11:32 -0700

    add configuration

---
+++ lib/tutum.rb
@@ -0,0 +1,48 @@
+class Tutum
+  attr_reader :username, :api_key
+
+  def initialize(username, api_key)
+    @username = username
+    @api_key = api_key
+  end
+
+  def headers
+    {
+      "Authorization" => "ApiKey #{@username}:#{@api_key}",
+      "Accept" => "application/json",
+      "Content-Type" => "application/json"
+    }
+  end
+
+  def actions
+    @actions ||= TutumContainers.new(headers)
+  end
+
+  def containers
+    @containers ||= TutumContainers.new(headers)
+  end
+
+  def images
+    @images ||= TutumImages.new(headers)
+  end
+
+  def node_clusters
+    @node_clusters ||= TutumNodeClusters.new(headers)
+  end
+
+  def node_types
+    @node_types ||= TutumNOdeTypes.new(headers)
+  end
+
+  def nodes
+    @nodes ||= TutumNodes.new(headers)
+  end
+
+  def providers
+    @providers ||= TutumProviders.new(headers)
+  end
+
+  def services
+    @services ||= TutumServices.new(headers)
+  end
+end
---
+++ spec/tutum_spec.rb
@@ -0,0 +1,20 @@
+require_relative './spec_helper'
+
+test_username = ENV['TUTUM_USERNAME']
+test_api_key = ENV['TUTUM_API_KEY']
+
+describe Tutum do
+  subject do
+    Tutum.new(test_username , test_api_key)
+  end
+
+  it "has a username and apikey" do
+    expect(subject.username).to eq(test_username )
+    expect(subject.api_key).to eq(test_api_key )
+  end
+
+  it "compiles headers" do
+    expect(subject.headers["Authorization"].length).to be > (1)
+    expect(subject.headers["Accept"]).to eq("application/json")
+  end
+end
